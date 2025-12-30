6af7bcfd6bfcb4b48a1ed2d8dcbf2a86bc71b9e2
Ashok kata <akata@onedoor.com>
2025-12-30 12:30:48 -0500

    fix: configure Next.js standalone mode for Amplify deployment

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,6 +7,7 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   // Disable image optimization for Amplify hosting
   images: {
     unoptimized: true,
